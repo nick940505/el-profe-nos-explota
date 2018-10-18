6d07802e403a020c8407e8ba688ba6f4cd70ec8a
nick940505 <u1802078@unimilitar.edu.co>
2018-10-17 21:02:46 -0500

    nuevo controlador article y rutas

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'crud/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles
+  root 'welcome#index'
 end
