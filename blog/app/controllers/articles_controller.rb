d004f66b5e0bea8f610ef4ec6da3ad4f0f9d9e02
nick940505 <u1802078@unimilitar.edu.co>
2018-10-20 22:06:10 -0500

    action show

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -3,6 +3,23 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        render plain: params[:article].inspect
-    end
+        @article = Article.new(article_params)
+       
+        @article.save
+        redirect_to @article
+      end
+
+      
+
+def show
+  @article = Article.find(params[:id])
+end
+
+
+       
+      private
+        def article_params
+          params.require(:article).permit(:tittle, :text)
+        end
+            
 end
